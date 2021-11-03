c5a9f980a12b97c8b14b298d14ce82cad9d97bb2
Riviere Fougy <rivierefougy@gmail.com>
2021-11-03 18:12:24 -0400

    Added temporary title

--- src/App.js
+++ src/App.js
@@ -6,25 +6,28 @@ import Box from "./components/Box";
 function App() {
   return (
     <>
-      <Canvas
-        dpr={[1, 2]}
-        shadows
-        gl={{ alpha: false }}
-        camera={{ position: [-2, 1, 7], fov: 50 }}
-      >
-        <color attach="background" args={["#ffffff"]} />
-        <hemisphereLight intensity={1} />
-        <spotLight
-          position={[5, 5, 5]}
-          angle={0.75}
-          penumbra={1}
-          intensity={1}
-          castShadow
-          shadow-mapSize-width={1028}
-          shadow-mapSize-height={1028}
-        />{" "}
-        <Box />
-      </Canvas>
+      <h1>kyo͞ob</h1>
+      <>
+        <Canvas
+          dpr={[1, 2]}
+          shadows
+          gl={{ alpha: false }}
+          camera={{ position: [-2, 1, 7], fov: 50 }}
+        >
+          <color attach="background" args={["#ffffff"]} />
+          <hemisphereLight intensity={1} />
+          <spotLight
+            position={[5, 5, 5]}
+            angle={0.75}
+            penumbra={1}
+            intensity={1}
+            castShadow
+            shadow-mapSize-width={1028}
+            shadow-mapSize-height={1028}
+          />{" "}
+          <Box />
+        </Canvas>
+      </>
     </>
   );
 }
