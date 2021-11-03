27793674ade910a7ee5b400acbdedd1b00d6c0c1
Riviere Fougy <rivierefougy@gmail.com>
2021-11-03 15:57:49 -0400

    Changed speed of cube rotation

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,7 +3,7 @@ import { useFrame } from "@react-three/fiber";
 
 const Box = () => {
   const mesh = useRef(null);
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.005));
   return (
     <mesh ref={mesh}>
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
