5936a4d89470e652c60d8673f481e6877163dc10
Riviere Fougy <rivierefougy@gmail.com>
2021-11-02 21:27:57 -0400

    App.js: created initial cube

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import "./App.css";
+import "./App.scss";
 import { Canvas } from "@react-three/fiber";
 
 function App() {
-  return <Canvas></Canvas>;
+  return (
+    <>
+      <Canvas>
+        <mesh>
+          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+          <meshStandardMaterial attach="material" />
+        </mesh>
+      </Canvas>
+    </>
+  );
 }
 
 export default App;
