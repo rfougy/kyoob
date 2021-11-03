52813e86a31b2e15cd30502a57c2b146053eeb49
Riviere Fougy <rivierefougy@gmail.com>
2021-11-02 21:41:11 -0400

    moved Box component to seperate file via components directory

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./App.scss";
-import { Canvas, useFrame } from "@react-three/fiber";
-
-const Box = () => {
-  const mesh = useRef(null);
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
-  return (
-    <mesh ref={mesh}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" />
-    </mesh>
-  );
-};
+import { Canvas } from "@react-three/fiber";
+import Box from "./components/Box";
 
 function App() {
   return (
---
+++ src/components/Box.js
@@ -0,0 +1,15 @@
+import React, { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+const Box = () => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" />
+    </mesh>
+  );
+};
+
+export default Box;
