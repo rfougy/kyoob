75de7ee0000f041901731903182e57fe1d080a55
Riviere Fougy <rivierefougy@gmail.com>
2021-11-04 16:52:55 -0400

    replaced theme with cssbaseline

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Header from "./components/Header";
 import HomePage from "./components/3DComponents/HomePage/HomePage";
+import { CssBaseline } from "@mui/material";
 
-import theme from "./theme";
-import { ThemeProvider } from "@mui/material";
+// import theme from "./theme";
+// import { ThemeProvider } from "@mui/material";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <>
+      {/* <ThemeProvider theme={theme}> */}
+      <CssBaseline />
       <Header />
       <HomePage />
-    </ThemeProvider>
+      {/* </ThemeProvider> */}
+    </>
   );
 }
 
