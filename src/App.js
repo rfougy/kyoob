745172f698f699620f02c52aa79c42a0cea632a2
Riviere Fougy <rivierefougy@gmail.com>
2021-11-04 14:56:59 -0400

    replaced previous plane with gridHelper component, removed several lighting settings

--- src/App.js
+++ src/App.js
@@ -13,32 +13,17 @@ function App() {
           dpr={[1, 2]}
           shadows
           gl={{ alpha: false }}
-          camera={{ position: [-2, 1, 7], fov: 50 }}
+          camera={{ position: [-5, 2, 10], fov: 60 }}
         >
           <color attach="background" args={["#ffffff"]} />
           <OrbitController />
           <ambientLight intensity={0.3} />
-          <directionalLight
-            position={[0, 10, 0]}
-            intensity={1.5}
-            shadow-mapSize-width={1024}
-            shadow-mapSize-height={1024}
-            shadow-camera-far={50}
-            shadow-camera-left={-10}
-            shadow-camera-right={10}
-            shadow-camera-top={10}
-            shadow-camera-bottom={-10}
-          />
+          <directionalLight position={[0, 10, 0]} intensity={1.5} />
           <pointLight position={[-10, 0, -20]} intensity={0.5} />
           <pointLight position={[0, -10, 0]} intensity={1.5} />
 
           <Box />
-          <group>
-            <mesh rottion={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
-              <planeBufferGeometry attach="geometry" args={[10, 10]} />
-              <meshStandardMaterial wireframe color={"black"} />
-            </mesh>
-          </group>
+          <gridHelper position={[0, -1, 0]} args={[100, 100, `gray`, `gray`]} />
         </Canvas>
       </>
     </>
