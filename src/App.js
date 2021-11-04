a04a713d6a795d226c821f1a6885c06993f754df
Riviere Fougy <rivierefougy@gmail.com>
2021-11-04 15:46:01 -0400

    created appbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 import HomePage from "./components/3DComponents/HomePage/HomePage";
 
 function App() {
   return (
     <>
+      <Header />
       <HomePage />
     </>
   );
