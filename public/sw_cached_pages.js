cb91783e4d5c71f978479a590a893471ab855b2a
Riviere Fougy <rivierefougy@gmail.com>
2021-11-06 01:06:31 -0400

    deleted offline.html, changes mades to service worker event listeners via sw_cached_pages.js

--- public/sw_cached_pages.js
+++ public/sw_cached_pages.js
@@ -1,28 +1,9 @@
 const self = this;
 const CACHE_NAME = "version-1";
-const urlsToCache = ["index.html", "offline.html"];
 
 self.addEventListener("install", (event) => {
-  console.log("INSTALL");
-  event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        console.log("Opened cache");
-
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => self.skipWaiting())
-  );
+  console.log("INSTALLED");
 });
-
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then(() => {
-//       return fetch(event.request).catch(() => caches.match("offline.html"));
-//     })
-//   );
-// });
 
 self.addEventListener("activate", (event) => {
   console.log("ACTIVATED");
@@ -38,3 +19,59 @@ self.addEventListener("activate", (event) => {
     )
   );
 });
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, resClone);
+        });
+        return res;
+      })
+      .catch((err) => caches.match(event.request))
+      .then((res) => res)
+  );
+});
+
+///////////////////////////////////////////////////////////////
+
+// const self = this;
+// const CACHE_NAME = "version-1";
+// const urlsToCache = ["index.html", "offline.html"];
+
+// self.addEventListener("install", (event) => {
+//   console.log("INSTALLED");
+//   event.waitUntil(
+//     caches
+//       .open(CACHE_NAME)
+//       .then((cache) => {
+//         console.log("Opened cache");
+
+//         return cache.addAll(urlsToCache);
+//       })
+//       .then(() => self.skipWaiting())
+//   );
+// });
+
+// self.addEventListener("activate", (event) => {
+//   console.log("ACTIVATED");
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) =>
+//       Promise.all(
+//         cacheNames.map((cache) => {
+//           if (cache !== CACHE_NAME) {
+//             return caches.delete(cache);
+//           }
+//         })
+//       )
+//     )
+//   );
+// });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     fetch(event.request).catch(() => caches.match(event.request))
+//   );
+// });
