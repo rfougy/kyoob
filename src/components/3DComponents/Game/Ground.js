0d543245a1c9c6721ffd614bb2b3526f99fcb72f
Riviere Fougy <rivierefougy@gmail.com>
2022-01-15 11:20:17 -0500

    changes prop values for gridHelper

--- src/components/3DComponents/Game/Ground.js
+++ src/components/3DComponents/Game/Ground.js
@@ -2,12 +2,19 @@ import React from "react";
 import { usePlane } from "@react-three/cannon";
 
 export const Ground = (props) => {
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
+  const [ref] = usePlane(() => ({ ...props }));
 
   return (
     <mesh ref={ref} receiveShadow>
-      <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
-      <meshStandardMaterial attach="material" />
+      <gridHelper
+        ref={ref}
+        recieveShadow
+        position={[0, 9, 0]}
+        args={[100, 100, `gray`, `gray`]}
+      />
+
+      {/* <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
+      <meshStandardMaterial attach="material" /> */}
     </mesh>
   );
 };
